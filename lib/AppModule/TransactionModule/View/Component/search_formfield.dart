861f7b2ea54f0e6c1e50fad11218635bffbb1a60
awaisajmal22 <awais.ajmal@builtindevs.com>
2023-05-20 06:58:44 -0700

    main

--- lib/AppModule/TransactionModule/View/Component/search_formfield.dart
+++ lib/AppModule/TransactionModule/View/Component/search_formfield.dart
@@ -6,47 +6,33 @@ Widget searchFormField(
     {required TextEditingController controller,
     required VoidCallback searchCallback,
     required String hintText}) {
-  return Container(
-    padding: EdgeInsets.symmetric(horizontal: 2.h),
-    decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(
-              blurRadius: 15,
-              offset: const Offset(0, 8),
-              color: const Color(0xff00001A).withOpacity(0.1))
-        ]),
-    child: TextFormField(
-      controller: controller,
-      cursorColor: Colors.grey,
-      style: TextStyle(
-          fontSize: 15.sp, color: Colors.black, fontWeight: FontWeight.w400),
-      textAlign: TextAlign.left,
-      decoration: InputDecoration(
-          hintText: hintText,
-          hintStyle: TextStyle(
-              fontSize: 15.sp,
-              color: Colors.black,
-              fontWeight: FontWeight.w400),
-          prefixIcon: GestureDetector(
-            onTap: searchCallback,
-            child: Icon(Icons.search),
-          ),
+  return TextFormField(
+    controller: controller,
+    cursorColor: Colors.grey,
+    style: TextStyle(
+        fontSize: 15.sp, color: Colors.black, fontWeight: FontWeight.w400),
+    textAlign: TextAlign.left,
+    decoration: InputDecoration(
+        hintText: hintText,
+        hintStyle: TextStyle(
+            fontSize: 15.sp, color: Colors.black, fontWeight: FontWeight.w400),
+        prefixIcon: GestureDetector(
+          onTap: searchCallback,
+          child: Icon(Icons.search),
+        ),
 
-          // ),
+        // ),
 
-          fillColor: Colors.white,
-          filled: true,
-          border: OutlineInputBorder(
-              borderSide: const BorderSide(color: Colors.transparent),
-              borderRadius: BorderRadius.circular(10)),
-          focusedBorder: OutlineInputBorder(
-              borderSide: const BorderSide(color: Colors.transparent),
-              borderRadius: BorderRadius.circular(10)),
-          enabledBorder: OutlineInputBorder(
-              borderSide: const BorderSide(color: Colors.transparent),
-              borderRadius: BorderRadius.circular(10))),
-    ),
+        fillColor: Colors.white,
+        filled: true,
+        border: OutlineInputBorder(
+            borderSide: const BorderSide(color: Colors.black),
+            borderRadius: BorderRadius.circular(10)),
+        focusedBorder: OutlineInputBorder(
+            borderSide: const BorderSide(color: Colors.black),
+            borderRadius: BorderRadius.circular(10)),
+        enabledBorder: OutlineInputBorder(
+            borderSide: const BorderSide(color: Colors.black),
+            borderRadius: BorderRadius.circular(10))),
   );
 }
